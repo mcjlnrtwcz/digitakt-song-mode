c6a288448f7b641a350c85d290a1b45e6f5f9128
Maciej Lenartowicz <mcjlnrtwcz@gmail.com>
2019-01-25 23:51:34 +0100

    Select MIDI channel

--- digitakt-song-mode.py
+++ digitakt-song-mode.py
@@ -16,13 +16,11 @@ class SongModeGUI:
         self.sequencer = sequencer
 
         # Main window
-
-        self.root.geometry('320x160+0+0')
+        self.root.geometry('320x240+0+0')
         self.root.title('digitakt-song-mode')
         root.after(0, self.refresh_position)
 
         # Widgets
-
         self.position_label = tk.Label(
             self.root,
             text='Position: N/A',
@@ -38,8 +36,7 @@ class SongModeGUI:
         self.toggle_seq_btn.grid(row=0, column=1, sticky=tk.E, padx=8)
 
         # MIDI output selector
-
-        self.midi_out_name = tk.StringVar(self.root)  # TODO: as property?
+        self.midi_out_name = tk.StringVar(self.root)
 
         self.midi_out_frame = tk.Frame(self.root)
         self.midi_out_frame.grid(
@@ -62,20 +59,46 @@ class SongModeGUI:
         )
         self.midi_out_selector.grid(row=1, column=0, sticky=tk.W+tk.E)
 
-        # Open sequence
+        # MIDI channel selector
+        self.midi_channel = tk.IntVar(self.root)
 
+        self.midi_channel_frame = tk.Frame(self.root)
+        self.midi_channel_frame.grid(
+            row=2,
+            column=0,
+            columnspan=2,
+            sticky=tk.W+tk.E,
+            padx=8
+        )
+        self.midi_channel_frame.columnconfigure(0, weight=1)
+
+        self.midi_channel_label = tk.Label(
+            self.midi_channel_frame,
+            text='MIDI channel'
+        )
+        self.midi_channel_label.grid(row=0, column=0, sticky=tk.W)
+
+        self.midi_channel_choices = list(range(1, 17))
+        self.midi_channel_selector = tk.OptionMenu(
+            self.midi_channel_frame,
+            self.midi_channel,
+            *self.midi_channel_choices
+        )
+        self.midi_channel_selector.grid(row=1, column=0, sticky=tk.W+tk.E)
+
+        # Open sequence
         self.load_btn = tk.Button(
             self.root,
             text='Open sequence',
             command=self.load_file
         )
-        self.load_btn.grid(row=2, column=1, sticky=tk.E, padx=8)
+        self.load_btn.grid(row=3, column=1, sticky=tk.E, padx=8)
 
         # Configure grid
-
         self.root.rowconfigure(0, weight=1)
         self.root.rowconfigure(1, weight=1)
         self.root.rowconfigure(2, weight=1)
+        self.root.rowconfigure(3, weight=1)
         self.root.columnconfigure(0, weight=2)
         self.root.columnconfigure(1, weight=2)
 
@@ -88,8 +111,7 @@ class SongModeGUI:
                 print('get_position() returned None')
         else:
             self.position_label.config(text='Position: N/A')
-        self.position_label.update()  # TODO
-        root.after(42, self.refresh_position)
+        self.root.after(42, self.refresh_position)
 
     def load_file(self):
             path = filedialog.askopenfilename(
@@ -101,10 +123,10 @@ class SongModeGUI:
                 self.sequencer.load_sequence(json.load(sequence_file))
 
     def toggle_seq(self):
-        # TODO: Turn on/off refresh_position?
         if self.sequencer.is_playing:
             self.sequencer.stop()
         else:
+            self.sequencer.set_midi_channel(self.midi_channel.get())
             self.sequencer.set_output_port(self.midi_out_name.get())
             self.sequencer.start()
 
